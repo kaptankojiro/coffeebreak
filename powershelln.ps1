8b267a4af9ae53ace08597c1000c67a7a9678b24
Fatih <kaptankojiro@gmail.com>
2019-11-23 13:36:20 +0300

    Update powershelln.ps1

--- powershelln.ps1
+++ powershelln.ps1
@@ -1,13 +1,7 @@
 #Requires -RunAsAdministrator
+#http://woshub.com/popup-notification-powershell/
 
 Set-ExecutionPolicy bypass
-Write-Host "This simple applications show it's coffee break time! notification every hour."
-sleep 2
-Write-Host "It runs as a Windows scheduled task and you can delete it whenever you want."
-sleep 2
-
-Write-Host "Testing soundspeaker"
-sleep 2
 Add-Type -AssemblyName System.Windows.Forms
 $global:balmsg = New-Object System.Windows.Forms.NotifyIcon
 $path = (Get-Process -id $pid).Path
@@ -16,13 +10,13 @@ $balmsg.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Warning
 $balmsg.BalloonTipText = "It's time for a coffee break!!!"
 $balmsg.BalloonTipTitle = "Dear $Env:USERNAME"
 $balmsg.Visible = $true
-$balmsg.ShowBalloonTip(20000)
+$balmsg.ShowBalloonTip(30000)
 
 [console]::beep(500,500)
 [console]::beep(500,500)
 [console]::beep(500,500)
-[console]::beep(500,350)
-[console]::beep(500,150)
+[console]::beep(500,500)
+
 
 
 
