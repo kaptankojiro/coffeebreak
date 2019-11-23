dfa58460d31009bdb7759e240c79424cd13aed42
Fatih <kaptankojiro@gmail.com>
2019-11-23 13:38:04 +0300

    Update coffeedownloader.ps1

--- coffeedownloader.ps1
+++ coffeedownloader.ps1
@@ -2,11 +2,9 @@
 
 Set-ExecutionPolicy bypass
 Write-Host "This simple script shows --> it is coffee break time! <-- notification every hour."
-sleep 2
+sleep 1
 Write-Host "It runs as a Windows scheduled task and you can delete it whenever you want scheduled task as administrator."
-sleep 2
-
-
+sleep 1
 
 Write-Host "Creating a new folder if it is necessary..." -ForegroundColor red -BackgroundColor white
 
@@ -18,8 +16,9 @@ If(!(test-path $path))
 
 
 Write-Host "Clearing the folder if it is not empty..." -ForegroundColor red -BackgroundColor white
-Remove-Item –path C:\coffee* -Filter *test* -whatif
+Remove-Item â€“path C:\coffee* -Filter *test* -whatif
 
+Write-Host "Downloading..." -ForegroundColor red -BackgroundColor white
 $HTTP_Request = [System.Net.WebRequest]::Create('https://raw.githubusercontent.com/kaptankojiro/coffeebreak/master/powershelln.ps1')
 $HTTP_Response = $HTTP_Request.GetResponse()
 $HTTP_Status = [int]$HTTP_Response.StatusCode
@@ -34,11 +33,11 @@ Else {
    exit
 }
 $HTTP_Response.Close()
- 
-sleep 2
-
+Write-Host "Downloading process finished..." -ForegroundColor red -BackgroundColor white
+sleep 1
+Write-Host "Creating and triggering scheduled task " -ForegroundColor red -BackgroundColor white
 cmd /c SCHTASKS /Create /SC MINUTE /MO 60 /TN PowershellCoffeeTime /TR "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -WindowStyle Hidden -ExecutionPolicy Unrestricted -File C:\coffee\powershelln.ps1"
 Start-ScheduledTask -TaskName "PowershellCoffeeTime"
-
+sleep 2
 
  
